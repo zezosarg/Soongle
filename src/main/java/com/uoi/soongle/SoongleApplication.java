aac4d02622f76072dbb67a59d5024d21c9303047
zezosarg <cs04588@uoi.gr>
2023-04-02 18:06:35 +0300

    added spring initializr files

---
+++ src/main/java/com/uoi/soongle/SoongleApplication.java
@@ -0,0 +1,13 @@
+package com.uoi.soongle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoongleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoongleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uoi/soongle/SoongleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoi.soongle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoongleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

73f941da73e2c94e56b10c7ca93c30df7c7ae926
cse84588 <cse84588@opti7020ws02>
2023-04-04 16:52:37 +0300

    able to read csv's to list

--- src/main/java/com/uoi/soongle/controller/SoongleController.java
+++ src/main/java/com/uoi/soongle/controller/SoongleController.java
@@ -1,16 +1,38 @@
 package com.uoi.soongle.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.opencsv.CSVReader;
 
 @Controller
 public class SoongleController {
 	
 	 @RequestMapping("/home")
 	 public String getHome() {
-		 System.out.println("home sweet home");
+		 List<List<String>> records = new ArrayList<List<String>>();
+		 try {
+			 CSVReader csvReader = new CSVReader(new FileReader("data/corpus.csv"));
+		     String[] values = null;
+		     while ((values = csvReader.readNext()) != null) {
+		         records.add(Arrays.asList(values));
+		     }
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }
+		 
+//		 System.out.println(records.get(1).get(3));
+		 
+		 
+		 
 		 return "home";
 	 }
 

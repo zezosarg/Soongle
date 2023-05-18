66fcaa11f88e77f09cf8eb90454047e803bcbfff
Argyrios Zezos <zezosarg@protonmail.com>
2023-05-18 11:15:44 +0300

    Update etl.py

--- data/etl.py
+++ data/etl.py
@@ -4,6 +4,6 @@ df = pd.read_csv('spotify_millsongdata.csv') # parse csv to a dataframe
 
 df = df.drop('link', axis=1)    # drop link column
 
-df = df.sample(n = 40000) # pick n random samples
+df = df.sample(n = 10000) # pick n random samples MAX 40K
 
 df.to_csv('corpus.csv', header=False) # export to csv without header
